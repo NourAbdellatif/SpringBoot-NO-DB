db929353d7942b63b95478e9a0895f706ff771a1
Noureldin Mohamed <noor.abdelatif@gmail.com>
2021-08-15 18:09:41 +0200

    JPA configured
    -Added student repo class extends from JPA repo
    
    -made StudentRepo in service
    
    -added @autowired

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,22 +1,23 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 @Service
 public class StudentService {
+
+    private final StudentRepo studentRepo;
+
+
+    @Autowired
+    public StudentService(StudentRepo studentRepo) {
+        this.studentRepo = studentRepo;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Lighto",
-                        LocalDate.of(2000, Month.APRIL,20),
-                        "noor.abdelatif@gmail.com",
-                        21
-                )
-        );
+        return studentRepo.findAll();
     }
 }
