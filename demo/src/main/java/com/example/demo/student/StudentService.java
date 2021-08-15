ff27e842e8fad46039090bd6f2b1e0c59d083d12
Noureldin Mohamed <noor.abdelatif@gmail.com>
2021-08-15 17:39:33 +0200

    Spring Beans added
    @Autowired and @Service

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,13 +1,13 @@
 package com.example.demo.student;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
-
+@Service
 public class StudentService {
-    @GetMapping
     public List<Student> getStudents(){
         return List.of(
                 new Student(
