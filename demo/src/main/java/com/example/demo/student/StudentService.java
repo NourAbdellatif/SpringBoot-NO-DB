d9b1f33b3bf5a368cee290be3c5faed4e8fa9295
Noureldin Mohamed <noor.abdelatif@gmail.com>
2021-08-20 02:04:46 +0200

    PostMapping
    To save student request

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -20,4 +20,8 @@ public class StudentService {
     public List<Student> getStudents(){
         return studentRepo.findAll();
     }
+
+    public void addNewStudent(Student student) {
+        System.out.println(student);
+    }
 }
